4a671da7c8e5b68411775e621efc82256786c0e3
Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>
2020-05-11 15:47:06 +0100

    Match tf version 12 output

--- envs/jumpbox/bin/vars-from-terraform.sh
+++ envs/jumpbox/bin/vars-from-terraform.sh
@@ -5,20 +5,20 @@ cd $ROOT
 
 tfoutput="terraform output --state=envs/aws/terraform.tfstate"
 cat <<YAML
-access_key_id:     $($tfoutput aws.creds.aws_access_key)
-secret_access_key: $($tfoutput aws.creds.aws_secret_key)
-default_key_name:  $($tfoutput aws.creds.key_name)
-private_key:       ../../$($tfoutput aws.creds.key_file)
-default_security_groups: [$($tfoutput aws.network.sg.dmz)]
-region:            $($tfoutput aws.network.region)
+access_key_id:     $($tfoutput aws_creds_aws_access_key)
+secret_access_key: $($tfoutput aws_creds_aws_secret_key)
+default_key_name:  $($tfoutput aws_creds_key_name)
+private_key:       ../../$($tfoutput aws_creds_key_file)
+default_security_groups: [$($tfoutput aws_network_sg_dmz)]
+region:            $($tfoutput aws_network_region)
 
-subnet_id:         $($tfoutput aws.network.dmz.subnet)
-az:                $($tfoutput aws.network.dmz.az)
-internal_cidr:     $($tfoutput aws.network.prefix).0.0/24
-internal_gw:       $($tfoutput aws.network.prefix).0.1
-internal_ip:       $($tfoutput aws.network.prefix).0.4
+subnet_id:         $($tfoutput aws_network_dmz_subnet)
+az:                $($tfoutput aws_network_dmz_az)
+internal_cidr:     $($tfoutput aws_network_prefix).0.0/24
+internal_gw:       $($tfoutput aws_network_prefix).0.1
+internal_ip:       $($tfoutput aws_network_prefix).0.4
 
-external_ip:       $($tfoutput box.jumpbox.public_ip)
+external_ip:       $($tfoutput box_jumpbox_public_ip)
 
 spot_bid_price: 10
 YAML
