928dfea8b2db180056f6d93b61b17392e2730526
Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>
2020-05-11 17:44:10 +0100

    Match tf version12 outputs

--- envs/bucc/bin/vars-from-terraform.sh
+++ envs/bucc/bin/vars-from-terraform.sh
@@ -6,18 +6,18 @@ cd $ROOT
 private_key_prefix=${private_key_prefix:-../../../..}
 tfoutput="terraform output --state=envs/aws/terraform.tfstate"
 cat <<YAML
-access_key_id:     $($tfoutput aws.creds.aws_access_key)
-secret_access_key: $($tfoutput aws.creds.aws_secret_key)
-default_key_name:  $($tfoutput aws.creds.key_name)
-private_key:       ${private_key_prefix}/$($tfoutput aws.creds.key_file)
-default_security_groups: [$($tfoutput aws.network.sg.dmz), $($tfoutput aws.network.sg.wide-open)]
-region:            $($tfoutput aws.network.region)
+access_key_id:     $($tfoutput aws_creds_aws_access_key)
+secret_access_key: $($tfoutput aws_creds_aws_secret_key)
+default_key_name:  $($tfoutput aws_creds_key_name)
+private_key:       ${private_key_prefix}/$($tfoutput aws_creds_key_file)
+default_security_groups: [$($tfoutput aws_network_sg_dmz), $($tfoutput aws_network_sg_wide-open)]
+region:            $($tfoutput aws_network_region)
 
-subnet_id:         $($tfoutput aws.network.private.subnet)
-az:                $($tfoutput aws.network.private.az)
-internal_cidr:     $($tfoutput aws.network.prefix).1.0/24
-internal_gw:       $($tfoutput aws.network.prefix).1.1
-internal_ip:       $($tfoutput aws.network.prefix).1.4
+subnet_id:         $($tfoutput aws_network_private_subnet)
+az:                $($tfoutput aws_network_private_az)
+internal_cidr:     $($tfoutput aws_network_prefix).1.0/24
+internal_gw:       $($tfoutput aws_network_prefix).1.1
+internal_ip:       $($tfoutput aws_network_prefix).1.4
 
 director_name: bucc-walk-thru
 
--- envs/bucc/cloud-config-operators/vars-from-terraform.sh
+++ envs/bucc/cloud-config-operators/vars-from-terraform.sh
@@ -5,11 +5,11 @@ cd $ROOT
 
 tfoutput="terraform output --state=envs/aws/terraform.tfstate"
 cat <<YAML
-az:            $($tfoutput aws.network.private.az)
-subnet_id:     $($tfoutput aws.network.private.subnet)
-internal_cidr: $($tfoutput aws.network.prefix).1.0/24
-internal_gw:   $($tfoutput aws.network.prefix).1.1
+az:            $($tfoutput aws_network_private_az)
+subnet_id:     $($tfoutput aws_network_private_subnet)
+internal_cidr: $($tfoutput aws_network_prefix).1.0/24
+internal_gw:   $($tfoutput aws_network_prefix).1.1
 
-dmz_subnet_id:           $($tfoutput aws.network.dmz.subnet)
-cf_services_subnet_id:   $($tfoutput aws.network.cf-services.subnet)
+dmz_subnet_id:           $($tfoutput aws_network_dmz_subnet)
+cf_services_subnet_id:   $($tfoutput aws_network_cf-services_subnet)
 YAML
